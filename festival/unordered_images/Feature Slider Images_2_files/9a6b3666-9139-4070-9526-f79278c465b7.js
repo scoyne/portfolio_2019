026a6d98f5d8765198842769ec6f4a96e12c7163
scoyne <scoyne@bw.edu>
2019-09-07 09:01:29 -0400

    initial commit - uploaded files

---
+++ festival/unordered_images/Feature Slider Images_2_files/9a6b3666-9139-4070-9526-f79278c465b7.js
@@ -0,0 +1,11 @@
+// Usersnap config - 2019-02-24 20:03:36
+!function(){
+var c=window._usersnapconfig||{},nc={"apiKey": "9a6b3666-9139-4070-9526-f79278c465b7", "labelRequired": false, "assignee": false, "colors": {"primary": "#005E99", "secondary": "#66AACB"}, "tools": ["comment", "pen", "highlight"], "btnText": "Feedback", "shortcut": false, "label": false, "theme": "ptiy", "valign": "middle", "hideTour": true, "emailBox": true, "emailRequired": false, "consoleRecorder": false, "commentBox": true, "commentRequired": false, "lang": "auto", "assigneeRequired": false, "usecase": "custom", "hostconfig": {"cdn": "//cdn.usersnap.com", "api": "https://api.usersnap.com", "homepage": "https://usersnap.com"}, "halign": "default", "mode": "report"};
+for(var e in nc)nc.hasOwnProperty(e)&&(void 0!==c[e]&&"apiKey"!==e||(c[e]=nc[e]));
+window._usersnapconfig=c;
+var s=document.createElement('script'),n=document.getElementsByTagName('script')[0],p=(window._usersnapconfig&&_usersnapconfig.hostconfig&&_usersnapconfig.hostconfig.cdn)||"//cdn.usersnap.com";
+s.type='text/javascript';
+s.async=!0;
+s.src=p+"/widget/loader.js";
+n.parentNode.insertBefore(s,n);
+}();
